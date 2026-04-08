263a6e2c318958a21f3a12a97694ea314f2313de
Your Name <youremail@example.com>
2026-04-08 11:33:10 +0900

    1

--- src/main/java/com/example/demo/controller/BoardController.java
+++ src/main/java/com/example/demo/controller/BoardController.java
@@ -1,37 +1,4 @@
 package com.example.demo.controller;
 
-import org.springframework.ui.Model;
-import com.example.demo.dto.Boarddto;
-import com.example.demo.service.BoardService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
-
-@Controller
-@RequiredArgsConstructor
-@RequestMapping("/board")
 public class BoardController {
-    private final BoardService boardService;
-
-    @GetMapping("/save")
-    public String saveForm() {
-        return "save";
-    }
-
-    @PostMapping("/save")
-    public String save(Boarddto boarddto) {
-        System.out.println("boarddto = " + boarddto);
-        boardService.save(boarddto);
-        return "redirect:/board/";
-    }
-    @GetMapping("/")
-    public String findAll(Model model) {
-        List<Boarddto> boarddtoList = boardService.findAll();
-        model.addAttribute("boardList", boarddtoList);
-        return "list";
-    }
 }
--- src/main/java/com/example/demo/service/BoardService.java
+++ src/main/java/com/example/demo/service/BoardService.java
@@ -1,31 +1,4 @@
 package com.example.demo.service;
 
-import com.example.demo.dto.Boarddto;
-import com.example.demo.entity.BoardEntity;
-import com.example.demo.repository.BoardRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Service
-@RequiredArgsConstructor
 public class BoardService {
-    private final BoardRepository boardRepository;
-
-    public void save(Boarddto boarddto) {
-        BoardEntity boardEntity = BoardEntity.toSaveEntity(boarddto);
-        boardRepository.save(boardEntity);
-    }
-
-    public List<Boarddto> findAll() {
-        List<BoardEntity> boardEntityList = boardRepository.findAll();
-        List<Boarddto> boarddtoList = new ArrayList<>();
-        for (BoardEntity boardEntity: boardEntityList) {
-            boarddtoList.add(Boarddto.toBoarddto(boardEntity));
-
-        }
-        return boarddtoList;
-    }
 }
